4b5e8b6a91ec3eb8dccdfbeb16b0e17f0469ecb3
CaiYiLiang <cherryliang0112@gmail.com>
2019-09-20 22:05:40 +0800

    Account page improve

--- src/components/account/index.tsx
+++ src/components/account/index.tsx
@@ -1,38 +1,59 @@
-
 import React, { PureComponent } from 'react';
 import 'antd/dist/antd.css';
-import {Container, AccountName, Bold} from './style';
+import { Container, AccountInfo, Text } from './style';
 import { InjectedAccount } from '../../types/type';
 import { Icon, Menu, Dropdown } from 'antd';
 
 type Props = {
-  accounts: InjectedAccount[],
-  mainAccount: InjectedAccount
-}
+  accounts: InjectedAccount[];
+  mainAccount: InjectedAccount;
+};
 
 class AccountPage extends PureComponent<Props> {
   render() {
     const { accounts, mainAccount } = this.props as Props;
+    const accountAmount = accounts.length;
+
+    if (accountAmount < 1) {
+      return (
+        <Container>
+          <Text>You don't have an account.</Text>
+          <Text>Please create an account for further process.</Text>
+        </Container>
+      );
+    }
 
     const menuItems = accounts.map((account, i) => {
-      return <Menu.Item key={i}>
-        {account.name}
-      </Menu.Item>
+      return (
+        <Menu.Item key={i} onClick={() => console.log(accounts[i])}>
+          {account.name}
+        </Menu.Item>
+      );
     });
 
-    const menu =  <Menu>{menuItems}</Menu>;
+    const menu = <Menu>{menuItems}</Menu>;
 
     return (
       <Container>
         <Dropdown overlay={menu}>
-          <a className="ant-dropdown-link" href="#">
-            Switch account <Icon type="down" />
+          <a className='ant-dropdown-link' href='#'>
+            Switch account <Icon type='down' />
           </a>
         </Dropdown>
-        <br/><br/>
-        <Icon type="user" />
-        <AccountName><Bold>Account name:</Bold> {mainAccount ? mainAccount.name : ''}</AccountName>
-        <div><Bold>Account address:</Bold> {mainAccount? mainAccount.address : ''}</div>
+        <br />
+        <br />
+        {mainAccount && mainAccount.name && (
+          <AccountInfo>
+            <Icon type='user' /> Account:
+            {mainAccount ? mainAccount.name : ''}
+          </AccountInfo>
+        )}
+        {mainAccount && mainAccount.address && (
+          <AccountInfo>
+            <Icon type='wallet' /> Address:
+            {mainAccount ? mainAccount.address : ''}
+          </AccountInfo>
+        )}
       </Container>
     );
   }
