4c51ee1021dfe641f76ac97e196db663c8b5fa2c
CaiYiLiang <cherryliang0112@gmail.com>
2019-09-20 22:38:02 +0800

    Account page improve

--- src/components/account/index.tsx
+++ src/components/account/index.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useState } from 'react';
 import 'antd/dist/antd.css';
 import { Container, AccountInfo, Text } from './style';
 import { InjectedAccount } from '../../types/type';
@@ -9,54 +9,56 @@ type Props = {
   mainAccount: InjectedAccount;
 };
 
-class AccountPage extends PureComponent<Props> {
-  render() {
-    const { accounts, mainAccount } = this.props as Props;
-    const accountAmount = accounts.length;
+const AccountPage: React.FunctionComponent<Props> = props => {
+  const [
+    selectedAccount,
+    setSelectedAccount
+  ] = useState<InjectedAccount | null>(null);
+  const { accounts, mainAccount } = props as Props;
+  const accountAmount = accounts.length;
 
-    if (accountAmount < 1) {
-      return (
-        <Container>
-          <Text>You don't have an account.</Text>
-          <Text>Please create an account for further process.</Text>
-        </Container>
-      );
-    }
-
-    const menuItems = accounts.map((account, i) => {
-      return (
-        <Menu.Item key={i} onClick={() => console.log(accounts[i])}>
-          {account.name}
-        </Menu.Item>
-      );
-    });
-
-    const menu = <Menu>{menuItems}</Menu>;
-
+  if (accountAmount < 1) {
     return (
       <Container>
-        <Dropdown overlay={menu}>
-          <a className='ant-dropdown-link' href='#'>
-            Switch account <Icon type='down' />
-          </a>
-        </Dropdown>
-        <br />
-        <br />
-        {mainAccount && mainAccount.name && (
-          <AccountInfo>
-            <Icon type='user' /> Account:
-            {mainAccount ? mainAccount.name : ''}
-          </AccountInfo>
-        )}
-        {mainAccount && mainAccount.address && (
-          <AccountInfo>
-            <Icon type='wallet' /> Address:
-            {mainAccount ? mainAccount.address : ''}
-          </AccountInfo>
-        )}
+        <Text>You don't have an account.</Text>
+        <Text>Please create an account for further process.</Text>
       </Container>
     );
   }
-}
+
+  const menuItems = accounts.map((account, i) => {
+    return (
+      <Menu.Item key={i} onClick={() => setSelectedAccount(accounts[i])}>
+        {account.name}
+      </Menu.Item>
+    );
+  });
+
+  const menu = <Menu>{menuItems}</Menu>;
+
+  const displayAccount = mainAccount || selectedAccount || accounts[0];
+
+  return (
+    <Container>
+      <Dropdown overlay={menu}>
+        <a className='ant-dropdown-link' href='#'>
+          Switch account <Icon type='down' />
+        </a>
+      </Dropdown>
+      <br />
+      <br />
+      {displayAccount && displayAccount.name && (
+        <AccountInfo>
+          <Icon type='user' /> Account: {displayAccount.name}
+        </AccountInfo>
+      )}
+      {displayAccount && displayAccount.address && (
+        <AccountInfo>
+          <Icon type='wallet' /> Address: {displayAccount.address}
+        </AccountInfo>
+      )}
+    </Container>
+  );
+};
 
 export default AccountPage;
--- src/components/sideMenu/index.tsx
+++ src/components/sideMenu/index.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import 'antd/dist/antd.css';
 import { Layout, Menu, Icon } from 'antd';
@@ -8,7 +7,7 @@ const { Sider } = Layout;
 
 class SideMenu extends React.Component {
   state = {
-    collapsed: false,
+    collapsed: false
   };
 
   onCollapse = (collapsed: any) => {
@@ -18,27 +17,31 @@ class SideMenu extends React.Component {
 
   render() {
     return (
-      <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-          <Logo>Foss</Logo>
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1">
-              <Icon type="team" />
-              <span>Account</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="account-book" />
-              <span>Beneficiaries</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-            <Icon type="pay-circle" />
-              <span>Payment</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="calendar" />
-              <span>Scheduling Pay</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
+      <Sider
+        collapsible
+        collapsed={this.state.collapsed}
+        onCollapse={this.onCollapse}
+      >
+        <Logo>Foss</Logo>
+        <Menu theme='dark' defaultSelectedKeys={['1']} mode='inline'>
+          <Menu.Item key='1'>
+            <Icon type='team' />
+            <span>Account</span>
+          </Menu.Item>
+          <Menu.Item key='2'>
+            <Icon type='account-book' />
+            <span>Beneficiaries</span>
+          </Menu.Item>
+          <Menu.Item key='3'>
+            <Icon type='pay-circle' />
+            <span>Payment</span>
+          </Menu.Item>
+          <Menu.Item key='4'>
+            <Icon type='calendar' />
+            <span>Scheduling Pay</span>
+          </Menu.Item>
+        </Menu>
+      </Sider>
     );
   }
 }
