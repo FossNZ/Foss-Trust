d3228576f6b91722548ec32444fe90f5d12bc120
CaiYiLiang <cherryliang0112@gmail.com>
2019-09-21 16:43:41 +0800

    Fix conflict

--- src/components/beneficiaries/index.tsx
+++ src/components/beneficiaries/index.tsx
@@ -4,11 +4,7 @@ import { InjectedAccount } from '../../types/type';
 import { Icon, Menu, Dropdown } from 'antd';
 import Wrapper from '../../components/Wrapper/index';
 import styled from 'styled-components';
-<<<<<<< HEAD
-import { Input, InputNumber, Form } from 'antd';
-=======
 import { Input, InputNumber } from 'antd';
->>>>>>> master
 
 const TitleWrapper = styled.div`
   display: flex;
