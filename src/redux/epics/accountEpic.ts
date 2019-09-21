b7bf1c57b9dc494dae4087c2129cb40eb937624b
Alex Zhen Wang <wangzhennz@gmail.com>
2019-09-21 20:26:35 +0800

    fix balance

--- src/redux/epics/accountEpic.ts
+++ src/redux/epics/accountEpic.ts
@@ -1,19 +1,32 @@
-import { ofType, ActionsObservable } from 'redux-observable';
+import { ofType, ActionsObservable, combineEpics, StateObservable } from 'redux-observable';
 import { AnyAction } from 'redux';
-import {  map } from 'rxjs/operators';
+import {  map, withLatestFrom } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import actions from '../actions';
 
-  const setAccountEpic = (
-    action$: ActionsObservable<AnyAction>
-  ): Observable<AnyAction> =>
-    action$.pipe(
-      ofType(actions.SET_MAIN_ACCOUNT),
-      map(() => {
-        return {
-          type: actions.FETCH_BALANCES
-        }
-      })
-    );
+const setAccountEpic = (
+  action$: ActionsObservable<AnyAction>
+): Observable<AnyAction> =>
+  action$.pipe(
+    ofType(actions.SET_MAIN_ACCOUNT),
+    map(() => {
+      return {
+        type: actions.FETCH_BALANCES
+      }
+    })
+  );
 
-export default setAccountEpic;
+const setAccountsEpic = (
+  action$: ActionsObservable<AnyAction>,
+): Observable<AnyAction> =>
+  action$.pipe(
+    ofType(actions.SET_ACCOUNTS),
+    map((action) => {
+      return {
+        type: actions.SET_MAIN_ACCOUNT,
+        payload: action.payload[0]
+      }
+    })
+  );
+
+export default combineEpics(setAccountEpic, setAccountsEpic);
